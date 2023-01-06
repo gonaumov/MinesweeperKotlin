6ccc70c3d06c834fea19cc07130f241faa361fa5
Georgi Naumov <gonaumov@gmail.com>
2023-01-06 10:11:35 +0200

    Stage 2/5: Flexible mines

--- src/main/kotlin/minesweeper/Main.kt
+++ src/main/kotlin/minesweeper/Main.kt
@@ -1,6 +1,9 @@
 package minesweeper
 
 fun main(args: Array<String>) {
+    println("How many mines do you want on the field?")
+    val minesNumber = readln().toInt()
     val minesweeperGame = MinesweeperGame(9, 9)
+    minesweeperGame.setMines(minesNumber)
     println(minesweeperGame.getBoard())
 }
--- src/main/kotlin/minesweeper/MinesweeperGame.kt
+++ src/main/kotlin/minesweeper/MinesweeperGame.kt
@@ -1,20 +1,31 @@
 package minesweeper
 
-class MinesweeperGame(private val columns: Int, rows: Int) {
+import kotlin.random.Random
+
+private const val EMPTY_CELL_VALUE = "."
+private const val MINED_CELL_VALUE = "X"
+
+class MinesweeperGame(private val columns: Int, private val rows: Int) {
     private val board = MutableList(rows) {
-            _: Int ->
         MutableList(columns) {
-            getFieldContent()
+            EMPTY_CELL_VALUE
         }
     }
 
-    private fun getFieldContent(): String {
-        return listOf(".", "X").random()
+    fun setMines(minesNumber: Int) {
+        var mines = minesNumber
+        do {
+            val x = Random.nextInt(0, columns)
+            val y = Random.nextInt(0, rows)
+            if (board[x][y] == EMPTY_CELL_VALUE) {
+                board[x][y] = MINED_CELL_VALUE
+                mines--
+            }
+        } while (mines != 0)
     }
 
     fun getBoard(): String {
-        return board.fold("") {
-                acc: String, strings: MutableList<String> ->
+        return board.fold("") { acc: String, strings: MutableList<String> ->
             acc + strings.joinToString("") + "\n"
         }
     }
